300a1097aaf19671c253c6d0ad02a95cad48bda4
Leandro Koller <lekoller@yahoo.com>
2022-05-21 11:52:55 -0300

    chore: Add auth module, controller and service.

--- disparo-pro-backend/src/app.module.ts
+++ disparo-pro-backend/src/app.module.ts
@@ -5,11 +5,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
 import { TypeOrmConfigService } from './database/typeorm.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     AccountsModule,
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
